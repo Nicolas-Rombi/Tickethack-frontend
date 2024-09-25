c1bc0125885001fc138981ebd625a140d0bade3e
Nicolas Rombi <nico461@hotmail.fr>
2024-09-25 16:16:11 +0200

    final

--- script3.js
+++ script3.js
@@ -1,1 +1,33 @@
-const cartPage = document.getElementById('my-bookings');
+const bookingPage = document.getElementById('my-bookings');
+fetch('http://localhost:3000/purchase')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.length > 0) {
+            data.forEach(booking => {
+                const bookingElement = document.createElement('div');
+                bookingElement.className = 'booking-item';
+                const departureMoment = moment(booking.date);
+                const now = moment();
+                const timeDifference = departureMoment.diff(now); 
+                let timeRemaining;
+
+                if (timeDifference > 0) {
+                    const hours = departureMoment.diff(now, 'hours');
+                    const minutes = departureMoment.diff(now, 'minutes') % 60;
+                    timeRemaining = `${hours}h ${minutes}m`;
+                } 
+
+                bookingElement.innerHTML = `
+                    <p>${booking.departure} > ${booking.arrival}</p>
+                    <p>${moment(booking.date).format('HH:mm')}</p>
+                    <p>${booking.price}€</p>
+                    <p>Départ dans : ${timeRemaining}</p>
+                `;
+                bookingPage.appendChild(bookingElement);
+            });
+        } else {
+            bookingPage.innerHTML = '<p>Aucune réservation trouvée.</p>';
+        }
+    })
+    
