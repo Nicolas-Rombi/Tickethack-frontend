5afdd8d7cce492f1d124e5a8a208752c59823b4b
Nicolas Rombi <nico461@hotmail.fr>
2024-09-25 11:22:12 +0200

    delete

--- script2.js
+++ script2.js
@@ -1,17 +1,31 @@
 const cartPage = document.getElementById('my-carts');
+let totalPrice = 0;
 
 const tripKeys = Object.keys(localStorage); 
 if (tripKeys.length > 0) {
     tripKeys.forEach(key => {
         const trip = JSON.parse(localStorage.getItem(key)); 
+       totalPrice += trip.price;
         const tripElement = document.createElement('div');
         tripElement.className = 'trip-item';
         tripElement.innerHTML = `
-            <p>${trip.departure}> ${trip.arrival}  ${moment(trip.date).format('HH:mm')}   ${trip.price}€</p><button type="submit" id="delete">x</button>`
+            <p>${trip.departure}> ${trip.arrival}  ${moment(trip.date).format('HH:mm')}   ${trip.price}€</p><button type="submit" id="${key}" class="delete">x</button>`
            
         cartPage.appendChild(tripElement);
+        
     });
+    const PrixTotal = document.getElementById('total');
+    PrixTotal.textContent = `Total : ${totalPrice}€`;
+    
 } else {
     cartPage.innerHTML = `<p>Aucun trajet trouvé dans le panier.</p>`;
 }
 
+const deleteButton = document.querySelectorAll('.delete');
+    deleteButton.forEach(button => {
+        button.addEventListener('click', function() {
+            const tripId = button.getAttribute('id');
+            console.log(tripId);
+            localStorage.removeItem(tripId);
+            location.reload()
+        })})
